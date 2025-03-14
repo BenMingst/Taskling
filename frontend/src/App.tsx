754516e01523a3c9ec43bd9fb9c2ab0c2d111f04
Chami888 <ch247021@ucf.edu>
2025-03-14 15:16:29 -0400

    Update App.tsx
    Updated to go to signup page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,18 @@
-function App() {
-  return (
-    <h1>Hello, World!</h1>
-  );
-}
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import SignUp from './pages/SignUp';
+import SignIn from './pages/SignIn';
+import Task from './pages/Tasks';
 
-export default App;
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<SignUp/>} />  {/* Default route */}
+      <Route path="/signup" element={<SignUp />} />
+      <Route path="/signin" element={<SignIn />} />
+      <Route path="/task" element={<Task />} />
+    </Routes>
+  );
+};
+
+export default App; // Default export here
