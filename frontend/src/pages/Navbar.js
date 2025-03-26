75b7afa3151bb3b5058556f5753fea16874fb9b6
belacile <belacile13@gmail.com>
2025-03-26 19:07:23 -0400

    Navbar.js now has the capabilities to be added to any page (although the routing still needs to be done)

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -2,8 +2,25 @@ export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="TamagoTitle">Taskling</a>
         <ul>
-            <li>
-                <a href="/"></a>
+            <li className="active">
+                <a href="/">
+                    <img className="icon" src="../assets/tamagoIcon.png"></img>
+                </a>
+            </li>
+            <li class="active">
+                <a href="/">
+                    <img className="icon" src="../assets/accountIcon.png"></img>
+                </a>
+            </li>
+            <li class="active">
+                <a href="/">
+                    <img className="icon" src="../assets/listIcon.png"></img>
+                </a>
+            </li>
+            <li class="active">
+                <a href="/">
+                    <img className="icon" src="../assets/shopIcon.png"></img>
+                </a>
             </li>
         </ul>
     </nav>
