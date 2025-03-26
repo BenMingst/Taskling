b6c5503e7839861872b9424f046267742feac378
belacile <belacile13@gmail.com>
2025-03-26 18:06:33 -0400

    Added Icon Assets

--- frontend/src/pages/Navbar.js
+++ frontend/src/pages/Navbar.js
@@ -1,5 +1,10 @@
 export default function Navbar() {
     return <nav className="nav">
         <a href="/" className="TamagoTitle">Taskling</a>
+        <ul>
+            <li>
+                <a href="/"></a>
+            </li>
+        </ul>
     </nav>
 }
