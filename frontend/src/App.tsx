64e79f0c24a54d02a893cb0a2b28da367d9f8d2c
Christian1137 <chris.y5@icloud.com>
2025-03-28 22:00:42 -0400

    update to app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,14 +3,18 @@ import { Routes, Route } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Tasks from './pages/Tasks';
+import Shop from './pages/Shop';
+import Tamago from './pages/tamago';
 
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<SignUp />} />  {/* Default route */}
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/task" element={<Tasks />} />
+      <Route path="/" element={<SignUp/>} />  {/* Default route */}
+      <Route path="/SignUp" element={<SignUp />} />
+      <Route path="SignIn" element={<SignIn />} />
+      <Route path="/Tasks" element={<Tasks/>} />
+      <Route path="/Shop" element={<Shop/>} />
+      <Route path="/tamago" element={<Tamago/>} />
     </Routes>
   );
 };
