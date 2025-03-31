2641820106d4cf0f496744c7bc27b23a6047876f
Caleb <belacile13@gmail.com>
2025-03-31 12:02:54 -0400

    Changed App.tsx and main.tsx syntax to React V7

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,25 @@
-// import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { useRoutes } from 'react-router-dom';
 import SignUp from './pages/SignUp';
 import SignIn from './pages/SignIn';
 import Tasks from './pages/Tasks';
 import Shop from './pages/Shop';
-import Tamago from './pages/Tamago'
+import Tamago from './pages/Tamago';
 import AdminItems from './pages/AdminItems';
 import AdminUsers from './pages/AdminUsers';
 
 const App = () => {
-  return (
-    <Routes>
-      <Route path="/" element={<SignUp/>} /> 
-      <Route path="/SignUp" element={<SignUp />} />
-      <Route path="SignIn" element={<SignIn />} />
-      <Route path="/Tasks" element={<Tasks/>} />
-      <Route path="/Shop" element={<Shop/>} />
-      <Route path="/Tamago" element={<Tamago/>} />
-      <Route path="/admin/items" element={<AdminItems/>} />
-      <Route path="/admin/users" element={<AdminUsers/>} />
-    </Routes>
-  );
+  const routes = useRoutes([
+    { path: "/", element: <SignUp /> }, // Default route
+    { path: "/SignUp", element: <SignUp /> },
+    { path: "/SignIn", element: <SignIn /> },
+    { path: "/Tasks", element: <Tasks /> },
+    { path: "/Shop", element: <Shop /> },
+    { path: "/Tamago", element: <Tamago /> },
+    { path: "/admin/items", element: <AdminItems /> },
+    { path: "/admin/users", element: <AdminUsers /> },
+  ]);
+
+  return routes;
 };
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,7 @@
-// import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import { BrowserRouter } from 'react-router-dom'; // Import React Router
-import App from './App'; // Import the App component
-
-// const rootElement = document.getElementById('root') as HTMLElement;
-
-// thisll create the root elecment, and the ! means that it has to exist. 
-// this should make it be routable both in production and development
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
