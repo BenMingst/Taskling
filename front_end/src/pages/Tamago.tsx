23b10353b513d046406e17920c3e7f6b9a6d0478
belacile <belacile13@gmail.com>
2025-04-06 12:05:07 -0400

    reverted unfinished Tamago.tsx

--- front_end/src/pages/Tamago.tsx
+++ front_end/src/pages/Tamago.tsx
@@ -2,11 +2,11 @@ import './style.css';
 import { useTamagoFunctions } from './TamagoFunctions';
 import tamagoImage from '../../public/assets/tamago1.png';
 import tamagoCookie from '../../public/assets/cookie.png';
-//import tamagoGreyCookie from '../../public/assets/cookie_no_eat.png';
+import tamagoGreyCookie from '../../public/assets/cookie_no_eat.png';
 
 
 const Tamago = () => {
-  const { petImageRef, cookieImageRef, doRockingAnim, doFeedingAnim } = useTamagoFunctions();
+  const { petImageRef, doRockingAnim } = useTamagoFunctions();
 
   return (
     <>
@@ -18,18 +18,6 @@ const Tamago = () => {
               id="PetImage"
               src={tamagoImage}
               alt="Pet"
-            />
-          </button>
-        </div>
-      
-        <div className="CookieBox">
-          <label id="CookieCount">0</label>
-          <button onClick={doFeedingAnim} className="CookieButton">
-            <img
-              ref={cookieImageRef}
-              id="CookieImage"
-              src={tamagoCookie}
-              alt="Cookie"
             />
           </button>
         </div>
