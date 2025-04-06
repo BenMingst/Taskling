28c74513e2ca2020ef0659ecc9cf84e77217a531
belacile <belacile13@gmail.com>
2025-04-06 12:03:08 -0400

    Added cookie styling to style.css

--- front_end/src/pages/tamagojs.js
+++ front_end/src/pages/tamagojs.js
@@ -1,26 +1,48 @@
 // Animation
 
 var petAnimPlaying = false;
+var numCookies = 500;
 
-function startRockingAnim(obj){
-    obj.style.animationPlayState = "running";
-    obj.style.animationIterationCount = "infinite";
+function updateNum(){
+    document.getElementById("CookieCount").textContent = numCookies;
 }
 
-function endRockingAnim(obj){
-    obj.style.animationPlayState = "paused";
+function startAnim(obj){
+    obj.style.animationPlayState = "running";
 }
 
 function doRockingAnim(){
     
+    // numCookies+=1;
+    // document.getElementById("CookieCount").textContent = numCookies;
+
     if (petAnimPlaying == false){
         petAnimPlaying = true;
         let pet = document.getElementById("PetImage");
-        startRockingAnim(pet);
+        pet.style.animationIterationCount = "infinite";
+        startAnim(pet);
         setTimeout(function(){
             pet.style.animationPlayState = "paused";
             pet.style.animationIterationCount = "0";
             petAnimPlaying = false;
         }, 1750);
+    }
+}
+
+function doFeedingAnim(){
+    if (numCookies > 0){
+
+        numCookies-=1;
+        document.getElementById("CookieCount").textContent = numCookies;
+
+        var newCookie = document.createElement("IMG");
+        newCookie.setAttribute("src", "../../public/assets/cookie.png");
+        newCookie.setAttribute("id", "CookieClone");
+        (document.getElementById("Cookie")).appendChild(newCookie);
+
+        startAnim(newCookie);
+        setTimeout(function(){
+            newCookie.remove();
+        }, 1700);
     }
 }
